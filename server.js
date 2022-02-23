e09342e563c52378abd33a64184386c3a8f89dd6
joshraiborde <joshraiborde@gmail.com>
2022-02-23 12:51:14 -0500

    228 handling Requests and responses, get

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(request, response){
+    response.send("<h1>helloworld</h1>")
+});
+
 app.listen(3000, function() {
     console.log("sever started on port 3000");
 });
